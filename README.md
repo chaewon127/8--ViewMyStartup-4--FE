a95e3f1fb3734cd2410a81a9e81de4f05a3e3d33
kmjerror <6945830a@gmail.com>
2025-08-27 14:46:24 +0900

    chore: 개발환경 세팅

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# 8--ViewMyStartup-4--FE
-Part2 4팀 초급 프로젝트 View My Startup 프론트엔드 레포지토리입니다.
+# React + Vite
+
+This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+
+Currently, two official plugins are available:
+
+- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react) uses [Babel](https://babeljs.io/) for Fast Refresh
+- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+
+## Expanding the ESLint configuration
+
+If you are developing a production application, we recommend using TypeScript with type-aware lint rules enabled. Check out the [TS template](https://github.com/vitejs/vite/tree/main/packages/create-vite/template-react-ts) for information on how to integrate TypeScript and [`typescript-eslint`](https://typescript-eslint.io) in your project.
+
+
+npm run dev
+# 브라우저 열기 → http://localhost:3000
+
+npm run build
+# dist/ 폴더 생성 → 배포용 정적 파일
---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import Navbar from './components/Navbar';
+// import Footer from './components/Footer';
+// import Layout from './components/Layout';
+// import HomePage from './pages/HomePage';
+// import ComparePage from './pages/ComparePage';
+// import InvestmentPage from './pages/InvestmentPage';
+// import CompareSelectPage from './pages/CompareSelectPage';
+// import CompareResultPage from './pages/CompareResultPage';
+// import CompanyDetailPage from './pages/CompanyDetailPage';
+// import LoginPage from './pages/LoginPage';
+// import JoinPage from './pages/JoinPage';
+
+// function App() {
+//   return (
+//     <Router>
+//       <Navbar /> {/* 공통 헤더 */}
+//       <main>
+//         <Routes>
+//           <Route element={<Layout />}>
+//             <Route index element={<HomePage />} /> {/* / */}
+//             <Route path="compare" element={<ComparePage />} />
+//             <Route path="investment" element={<InvestmentPage />} />
+//             <Route path="compare/select" element={<CompareSelectPage />} />
+//             <Route path="compare/result" element={<CompareResultPage />} />
+//             <Route path="company/:id" element={<CompanyDetailPage />} />
+//             <Route path="login" element={<LoginPage />} />
+//             <Route path="join" element={<JoinPage />} />
+//           </Route>
+//         </Routes>
+//       </main>
+//       <Footer /> {/* 공통 푸터 */}
+//     </Router>
+//   );
+// }
+function App() {
+  return (
+    <>
+      hello world
+    </>
+  )
+}
+export default App;
