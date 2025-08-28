f349610042e78d94eb069fd638482354f19c6c09
MyeongjuHong <hmjpearl@gmail.com>
2025-08-28 19:10:06 +0900

    chore: 필요없는 페이지 및 css 파일 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,6 @@ import Layout from './components/Layout/Layout';
 import HomePage from './pages/HomePage';
 import ComparePage from './pages/ComparePage';
 import InvestmentPage from './pages/InvestmentPage';
-import CompareSelectPage from './pages/CompareSelectPage';
-import CompareResultPage from './pages/CompareResultPage';
 import CompanyDetailPage from './pages/CompanyDetailPage';
 import LoginPage from './pages/LoginPage';
 import JoinPage from './pages/JoinPage';
@@ -21,8 +19,6 @@ function App() {
           <Route index element={<HomePage />} />
           <Route path="compare" element={<ComparePage />} />
           <Route path="investment" element={<InvestmentPage />} />
-          <Route path="compare/select" element={<CompareSelectPage />} />
-          <Route path="compare/result" element={<CompareResultPage />} />
           <Route path="company/:id" element={<CompanyDetailPage />} />
           <Route path="login" element={<LoginPage />} />
           <Route path="join" element={<JoinPage />} />
