03ddc5b137c059c77c645ca13cc95c32a49c6440
MyeongjuHong <hmjpearl@gmail.com>
2025-08-28 19:12:41 +0900

    feat: 기업 비교 페이지, 기업 추가 컨테이너, 카드 컴포넌트 추가

---
+++ src/components/CardAdded.jsx
@@ -0,0 +1,15 @@
+function CardAdded({ companyName, companyCategory}) {
+  return (
+    <>
+      <div>
+        <button>닫기</button>
+      {/* <MinusIcon /> */}
+      </div>
+      <img />
+      <div>{companyName}</div>
+      <div>{companyCategory}</div>
+    </>
+  );
+}
+
+export default CardAdded;
--- src/pages/ComparePage/index.jsx
+++ src/pages/ComparePage/index.jsx
@@ -1,5 +1,13 @@
 import React from "react";
+import CardContainer from "../../components/CardContainer";
+import LargeButton from "../../components/LargeButton";
 
 export default function ComparePage() {
-  return <div className="comparepage" />
+  return (
+    <>
+      <CardContainer title={'나의 기업을 선택해주세요!'} btnName={'전체 초기화'} companyName={'코드잇'} companyCategory={'에듀테크'} />
+      <CardContainer title={'어떤 기업이 궁금하세요?'} desc={'(최대 5개)'} btnName={'기업 추가하기'} companyName={'코드잇'} companyCategory={'에듀테크'} />
+      <LargeButton>기업 비교하기</LargeButton>
+    </>
+  );
 }
