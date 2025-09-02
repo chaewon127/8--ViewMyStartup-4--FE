72e6454f5b2c90c6d5335e2b55360dc146a79983
MyeongjuHong <hmjpearl@gmail.com>
2025-09-02 18:42:20 +0900

    feat: mock data추가

--- src/pages/ComparePage/index.jsx
+++ src/pages/ComparePage/index.jsx
@@ -4,22 +4,51 @@ import LargeButton from "../../components/LargeButton";
 import "./ComparePage.css";
 
 export default function ComparePage() {
+  const MOCK_MY = [
+    {
+      url: "src/assets/default.svg",
+      name: "코드스테이츠",
+      category: "에듀테크",
+    },
+  ];
+  const MOCK_COMPARE = [
+    {
+      url: "src/assets/default.svg",
+      name: "코드스테이츠",
+      category: "에듀테크",
+    },
+    {
+      url: "src/assets/default.svg",
+      name: "스파르타코딩클럽",
+      category: "에듀테크",
+    },
+    { url: "src/assets/default.svg", name: "앨리스", category: "에듀테크" },
+    {
+      url: "src/assets/default.svg",
+      name: "아이헤이트플라잉버그즈버그즈버그즈",
+      category: "에듀테크",
+    },
+    { url: "src/assets/default.svg", name: "메스프레소", category: "에듀테크" },
+  ];
+
   return (
-    <>
+    <div className="compare-page">
       <CardContainer
         title={"나의 기업을 선택해주세요!"}
         btnName={"전체 초기화"}
-        companyName={"코드잇"}
-        companyCategory={"에듀테크"}
+        companyList={MOCK_MY}
+        isData={false}
       />
       <CardContainer
         title={"어떤 기업이 궁금하세요?"}
         desc={"(최대 5개)"}
         btnName={"기업 추가하기"}
-        companyName={"코드잇"}
-        companyCategory={"에듀테크"}
+        companyList={MOCK_COMPARE}
+        isData={true}
       />
-      <LargeButton>기업 비교하기</LargeButton>
-    </>
+      <div className="btn-compare">
+        <LargeButton>기업 비교하기</LargeButton>
+      </div>
+    </div>
   );
 }
