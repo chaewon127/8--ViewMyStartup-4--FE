4898705f8b810f13b84cfad371f820e31adc9fb6
kmjerror <6945830a@gmail.com>
2025-09-02 19:29:36 +0900

    feat: 공통컴포넌트 버튼추가

--- src/components/MediumButton.jsx
+++ src/components/MediumButton.jsx
@@ -1,12 +1,34 @@
-function MediumButton ({ children }) {
+import React from 'react';
+import './Button.css';
+
+export default function MediumButton({
+  childern,
+  variant = 'active',
+  disabled = false,
+  fullWidth = false,
+  type = 'button',
+  onClick,
+  className = '',
+}) {
+  const classes = [
+    'vms-btn',
+    'vms-btn--md',
+    `vms-btn--md-${variant}`,
+    fullWidth ? 'is-block' : '',
+    disabled ? 'is-disabled' : '',
+    className,
+  ]
+    .filter(Boolean)
+    .join(' ');
+
   return (
-    <>
-      <button>
-        {/* <LoadingIcon/> */}
-        {children}
-        </button>
-    </>
+    <button
+      type={type}
+      className={classes}
+      disabled={disabled}
+      onClick={onClick}
+    >
+      <span className='vms-btn__label'>{childern}</span>
+    </button>
   );
 }
-
-export default MediumButton;
