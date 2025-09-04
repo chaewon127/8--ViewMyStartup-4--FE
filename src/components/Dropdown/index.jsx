ed72f69a6e9a8c9cdafbdf5c636492805757bb0d
MyeongjuHong <hmjpearl@gmail.com>
2025-09-04 15:34:00 +0900

    feat: dropdown 공통 컴포넌트 추가

--- src/components/Dropdown/index.jsx
+++ src/components/Dropdown/index.jsx
@@ -1,10 +1,17 @@
-function Dropdown({ list = [] }) {
+function Dropdown({ value, onChange, options = [], disabled = false }) {
   return (
     <>
-      <select>
-        {list.map((el) => (
-          <option key={el.id} value={list.value}>
-            {el.name}
+      <select
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
+        disabled={disabled || options.length === 0} // api 호출 전까지 의도적으로 비활성화
+      >
+        {options.map((option) => (
+          <option
+            key={option.id || option.value || option.name}
+            value={option.value}
+          >
+            {option.name}
           </option>
         ))}
       </select>
